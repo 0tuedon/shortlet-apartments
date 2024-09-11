6310ae8baa16c27d5f3eb5cc5f606bf8a2c8920f
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-09-11 14:29:06 +0100

    feat: chaanged porthacourt spelling

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -19,7 +19,7 @@ export default function ShortletLanding() {
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
           <MapPin className="h-6 w-6 mr-2" />
-          <span className="font-bold">Shortlet Porthacourt </span>
+          <span className="font-bold">Shortlet Portharcourt</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
@@ -212,16 +212,8 @@ export default function ShortletLanding() {
       </main>
       <footer  className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500">
-          © 2024 Shortlet Porthacourt. All rights reserved.
+          © 2024 Shortlet Portharcourt All rights reserved.
         </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Terms of Service
-          </Link>
-          <Link className="text-xs hover:underline underline-offset-4" href="#">
-            Privacy
-          </Link>
-        </nav>
       </footer>
     </div>
   );
